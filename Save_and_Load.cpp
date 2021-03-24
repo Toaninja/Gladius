6420bd01e389c66f8e0da5dc1c45a3dc9344e40a
E-Fischer-Repo <72356018+E-Fischer-Repo@users.noreply.github.com>
2021-03-24 16:58:58 -0400

    Removed: "Test.c". Added: "Save_and_Load.cpp", and "Save_and_Load.h"
    Adding some files with simple structure to the Save&Load branch. Removing the Test.c that was used to test commit/push/pull functionality. Adding a cpp file and header file for the Save&Load functions.

---
+++ Save_and_Load.cpp
@@ -0,0 +1,25 @@
+/* File: Save_and_Load.cpp
+ * Author: Eric Fischer
+ * Date: 
+ * Description: Holds function definitions for the Save_and_Load module
+ */
+ 
+ #include "Save_and_Load.h"
+ 
+ /* <Placeholder text-will add documentation used in previously submitted docs>
+ */
+ bool manualSave(){
+	 
+ }
+ 
+ /* <Placeholder text-will add documentation used in previously submitted docs>
+ */
+ bool manualLoad(){
+	 
+ }
+ 
+ /* <Time & resource permitting this function will be developed>
+ bool autoSave(){
+ 
+ }
+ */
---
+++ Save_and_Load.h
@@ -0,0 +1,12 @@
+ /* File: Save_and_Load.h
+ * Author: Eric Fischer
+ * Date: 
+ * Description: This file will hold Save_and_Load.cpp function definitions, include statements, and any potential global variables
+ */
+ 
+ #include <string.h>
+ 
+ bool manualSave();
+ bool manualLoad();
+ 
+ // bool autoSave();
