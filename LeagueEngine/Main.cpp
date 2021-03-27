1308ebc71f44cc5d6f2073743ddbddda8655ac46
nsmangat <nsmangat990@gmail.com>
2021-03-27 19:42:08 -0400

    Made get functions for gladiator attributes for testing

--- LeagueEngine/Main.cpp
+++ LeagueEngine/Main.cpp
@@ -1,4 +1,14 @@
-
+//Notes:
+//detect if player has played against a gladiator in the league? 
+//	Use flag? 
+//2D array can be player with W/L against each gladiator 
+//
+//
+//Enhancements:
+//round robin to random W/L for random gladiators
+//
+//Bugs/challenges:
+//Can't use command line arg as define feature 
 
 #include <iostream>
 #include <time.h>
@@ -54,7 +64,6 @@ public:
 	{
 		this->name = nameInput;
 	}
-	//getName for testing
 	
 	void setAttributes(int HP, float attack, float strength, float defence)
 	{
@@ -62,6 +71,28 @@ public:
 		*this->attack = attack;
 		*this->strength = strength;
 		*this->defence = defence;
+	}
+
+	//get functions used for testing
+	string getName()
+	{
+		return this->name;
+	}
+	int getHP()
+	{
+		return *this->HP;
+	}
+	float getAttack()
+	{
+		return *this->attack;
+	}
+	float getStrength()
+	{
+		return *this->strength;
+	}
+	float getDefence()
+	{
+		return *this->defence;
 	}
 
 	void updateGladiator();
@@ -110,11 +141,12 @@ Gladiator generatePlayer(Gladiator player, string name, int focusInput)
 	//1=attack focused
 	//2=strength focused
 	//3=defence focused
+	//adjust starting numbers and modifiers
 
 	player.setName(name);
 
-	int HP = (rand() % (10 - 7 + 1)) + 7;
-	float attack = ((rand() % (5 - 1 + 1)) + 1);
+	int HP = (rand() % (10 - 7 + 1)) + 7;				//7-10 HP
+	float attack = ((rand() % (5 - 1 + 1)) + 1);		//1-5 combat stats
 	float strength = (rand() % (5 - 1 + 1)) + 1;
 	float defence = (rand() % (5 - 1 + 1)) + 1;
 
@@ -126,7 +158,6 @@ Gladiator generatePlayer(Gladiator player, string name, int focusInput)
 	else if (focusInput == 2)
 	{
 		strength = strength * CREATE_FOCUS;
-
 	}
 
 	else
@@ -135,7 +166,6 @@ Gladiator generatePlayer(Gladiator player, string name, int focusInput)
 	}
 
 	player.setAttributes(HP, attack, strength, defence);
-
 }
 
 
@@ -146,12 +176,9 @@ int main(int argc, char* argv[])
 	Gladiator player;
 	//Gladiator* player = new Gladiator;
 	string name = "testname";
-	int focusInput;
-
+	int focusInput = 1; 
 
 	generatePlayer(player, name, focusInput);
 
-
-
-
+	return 0;
 }
