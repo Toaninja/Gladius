7bce543f76ded8bb0578c91fdbc106d8f7897a50
nsmangat <nsmangat990@gmail.com>
2021-03-28 02:06:19 -0400

    Fixed issue with allocating memory in creating gladiator

--- LeagueEngine/Main.cpp
+++ LeagueEngine/Main.cpp
@@ -2,7 +2,7 @@
 //detect if player has played against a gladiator in the league? 
 //	Use flag? 
 //2D array can be player with W/L against each gladiator 
-//
+//Add failure checks for memory allocation
 //
 //Enhancements:
 //round robin to random W/L for random gladiators
@@ -19,22 +19,27 @@ using namespace std;
 #define LEAGUE_SIZE //atoi(argv[1])				//10 gladiators per league always
 #define CREATE_FOCUS 1.5						//initial multiplier for when player creates their gladiator
 
-typedef struct Background
-{
-	string city; 
-	string trait;
-}background;
+//typedef struct Background
+//{
+//	string city; 
+//	string trait;
+//}background;
 
 
 class Gladiator
 {
 	string name;
-	Background background;
+	//Background background;
 
-	int* HP = new int;;
+	/*int* HP = new int;
 	float* attack = new float;
 	float* strength = new float;
-	float* defence = new float;
+	float* defence = new float;*/
+
+	int* HP;
+	float* attack;
+	float* strength;
+	float* defence;
 
 	int league;
 	int rank;
@@ -45,20 +50,40 @@ public:
 	Gladiator()
 	{
 
-		this->name = "";
-		this->background.city = "";
-		this->background.trait = "";
+		this->name = "Unknown";
+		//this->background.city = "Unknown";
+		//this->background.trait = "Unknown";
 
-		this->HP = 0;
-		this->attack = 0;
-		this->strength = 0;
-		this->defence = 0;
+		/*this->HP = new int;
+		this->attack = new float;
+		this->strength = new float;
+		this->defence = new float;*/
+
+		HP = new int;
+		attack = new float;
+		strength = new float;
+		defence = new float;
 
 		this->league = 0;
 		this->rank = 0;
 		this->wins = 0;
 		this->losses = 0;
 	}
+
+	/*Gladiator(string name, int HP, float attack, float strength, float defence)
+	{
+		this->name = name;
+
+		this->HP = new int;
+		this->attack = new float;
+		this->strength = new float;
+		this->defence = new float;
+
+		*this->HP = HP;
+		*this->attack = attack;
+		*this->strength = strength;
+		*this->defence = defence;
+	}*/
 
 	void setName(string nameInput)
 	{
@@ -67,11 +92,19 @@ public:
 	
 	void setAttributes(int HP, float attack, float strength, float defence)
 	{
+		/*this->HP = new int;
+		this->attack = new float;
+		this->strength = new float;
+		this->defence = new float;*/
+		
 		*this->HP = HP;
 		*this->attack = attack;
 		*this->strength = strength;
 		*this->defence = defence;
 	}
+
+	friend void generatePlayer(Gladiator player, string name, int focusInput);
+
 
 	//get functions used for testing
 	string getName()
@@ -84,6 +117,7 @@ public:
 	}
 	float getAttack()
 	{
+		//this->attack = new float;
 		return *this->attack;
 	}
 	float getStrength()
@@ -95,11 +129,18 @@ public:
 		return *this->defence;
 	}
 
-	void updateGladiator();
+	void updateGladiator()
+	{
+
+	}
 
 	~Gladiator()
 	{
-		delete HP, attack, strength, defence;
+		//delete HP, attack, strength, defence;
+		delete HP;
+		delete attack;
+		delete strength;
+		delete defence;
 	}
 
 };
@@ -133,17 +174,20 @@ public:
 		this->tier = tier;
 	}
 
-	void standingsUpdate();
+	void standingsUpdate()
+	{
+
+	}
 };
 
-Gladiator generatePlayer(Gladiator player, string name, int focusInput)
+void generatePlayer(Gladiator *player, string name, int focusInput)
 {
 	//1=attack focused
 	//2=strength focused
 	//3=defence focused
 	//adjust starting numbers and modifiers
 
-	player.setName(name);
+	player->setName(name);
 
 	int HP = (rand() % (10 - 7 + 1)) + 7;				//7-10 HP
 	float attack = ((rand() % (5 - 1 + 1)) + 1);		//1-5 combat stats
@@ -165,20 +209,22 @@ Gladiator generatePlayer(Gladiator player, string name, int focusInput)
 		defence = defence * CREATE_FOCUS;
 	}
 
-	player.setAttributes(HP, attack, strength, defence);
+	player->setAttributes(HP, attack, strength, defence);
+
 }
 
-
-int main(int argc, char* argv[])
+//int main(int argc, char* argv[])
+int main(void)
 {
 	srand(time(NULL));
 
-	Gladiator player;
-	//Gladiator* player = new Gladiator;
+	Gladiator* player = new Gladiator;
 	string name = "testname";
 	int focusInput = 1; 
 
 	generatePlayer(player, name, focusInput);
+	cout << player->getAttack() << endl;
+
 
 	return 0;
 }
