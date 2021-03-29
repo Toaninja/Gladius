d1c8458b5ea2c8e30c3911005afc848e2fc7b9bb
E-Fischer-Repo <72356018+E-Fischer-Repo@users.noreply.github.com>
2021-03-28 22:38:48 -0400

    Filled out autoSave functionality
    most of this functionality should work well for the autoSave. Issues found under the enhancement for the autosave will need to be addressed before it's fully implemented.

--- Save_and_Load.cpp
+++ Save_and_Load.cpp
@@ -17,7 +17,7 @@
 	 printf("Name your save file: ");
 	 fgets(input, 12, stdin); //get input for file name
 	 
-	 strcpy(filename, input);
+	 strcpy(filename, input); // this may include a null return character, may need to remove the last character on this string 
 	 strcat(filename, ".dat");
 	
 	 if(fp = fopen(filename, "w") != NULL){ //open file with input name
@@ -64,6 +64,25 @@
  
  /* <Time & resource permitting this function will be developed>
  bool autoSave(){
- 
+	 FILE* fp;
+	 char* input, filename;
+	 
+	 // find the number of the most recent auto_save_#.dat file
+	 // if it's number is 1, check to see if the next character is 0
+	 // if the next char is not 0, then increment the number by 1
+	 // if the next char is 0, then loop back to 1
+	
+	 if(fp = fopen(filename, "w") != NULL){ //open file with filename
+		//write data to file fprintf()
+		// Data to write:
+			//All gladiators in the Gladiator array
+			//struct of matches
+			//winLoss array
+	 } else {
+		return 0; // if fp == NULL -> return 0 for false / unsuccessful save
+	 }
+	 
+	 fclose(fp); //close file
+	 return 1; // return 1 for true / successful save
  }
  */
