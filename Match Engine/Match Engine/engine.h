98dfd04b6f904a31e62d8d3de081f46ffceacd84
Cosmin Ene <nationalsown@gmail.com>
2021-04-02 03:27:32 -0400

    Match Engine functionality expanded, created basis for combat, balance tweaks needed.

--- Match Engine/Match Engine/engine.h
+++ Match Engine/Match Engine/engine.h
@@ -40,47 +40,64 @@ public:
 	friend Gladiator combatLoop(Gladiator g1, Gladiator g2);
 };
 
-float roll(int x = 101) {
+float roll(int x = 101, int y = 100) {
 
 	srand(time(NULL));
-	float val = (rand() % x)/100;
+	float val = (rand() % x)/y;
 	return val;
 };
 
 int sign() {
 
+	int pos = 1;
+	int neg = -1;
+
 	srand(time(NULL));
 	int val = rand() % 2;
-	if (val == 0 || val == 1)
+	if (val == 0)
 	{
-		return -1;
+		return neg;
 	}
 	else
 	{
-		return 1;
+		return pos;
 	}
 
-}
+};
 
+//Attack is a percentage, strength is damage value, defence is blocking, HP is max health
 
 Gladiator combatLoop(Gladiator g1, Gladiator g2) {
 
+	//set up 2 values to represent initial HPs
+
 	if (g1.HP > 0 || g2.HP > 0)
 	{
-	
+		if (*g1.attack * roll(101, 1) > 0.5) {
+
+			float hit = *g1.strength + sign() * roll(*g1.strength/2);
+
+			float block = *g2.defence * roll(*g2.defence);
 
 
+
+
+			if (g2.HP <= 0) {
+				return g1;
+			}
+
+		};
 	}
 
-}
+};
 
 
 void engine(Gladiator g1, Gladiator g2) {
 
 	Gladiator winner, loser;
 
-	
 
 
 
-}
+
+};
--- Match Engine/Match Engine/training.cpp
+++ Match Engine/Match Engine/training.cpp
@@ -1,1 +1,15 @@
 #include "training.h"
+
+
+/*
+if win
+training x 1.0
+if loss
+training x 0.7
+
+if 1...
+if 2...
+if 3...
+
+
+*/
