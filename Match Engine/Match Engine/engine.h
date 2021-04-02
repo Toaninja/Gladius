4420d1f283c32e238cd498d7b7f1ddf14f3cf789
Cosmin Ene <nationalsown@gmail.com>
2021-04-02 02:52:52 -0400

    engine mostly built, story laid out
    -Modified Gladiator class for compatibility at a local level
    -implemented rudimentary engine harness and made several sub-functions for the engine
    -Designed the fight story layout

--- Match Engine/Match Engine/engine.h
+++ Match Engine/Match Engine/engine.h
@@ -1,4 +1,86 @@
 #pragma once
 
 #include <iostream>
+#include <random>
 using namespace std;
+
+class Gladiator
+{
+	string name;
+
+	int* HP;
+	float* attack;
+	float* strength;
+	float* defence;
+
+	int league;
+	int wins;
+	int losses;
+	int focus;				//1==attack, 2==strength, 3==defence
+	int matchMarker;
+
+public:
+	Gladiator();
+
+	void setName(string nameInput);
+	void setHP(int HP);
+	void setAttack(float attack);
+	void setStrength(float strength);
+	void setDefence(float defence);
+	void setWins(int wins);
+	void setLoss(int losses);
+	void setFocus(int focusInput);
+	void setAttributes(int HP, float attack, float strength, float defence);
+
+	void promote();
+	void demote();
+
+
+	friend void engine(Gladiator g1, Gladiator g2);
+	friend Gladiator combatLoop(Gladiator g1, Gladiator g2);
+};
+
+float roll(int x = 101) {
+
+	srand(time(NULL));
+	float val = (rand() % x)/100;
+	return val;
+};
+
+int sign() {
+
+	srand(time(NULL));
+	int val = rand() % 2;
+	if (val == 0 || val == 1)
+	{
+		return -1;
+	}
+	else
+	{
+		return 1;
+	}
+
+}
+
+
+Gladiator combatLoop(Gladiator g1, Gladiator g2) {
+
+	if (g1.HP > 0 || g2.HP > 0)
+	{
+	
+
+
+	}
+
+}
+
+
+void engine(Gladiator g1, Gladiator g2) {
+
+	Gladiator winner, loser;
+
+	
+
+
+
+}
--- Match Engine/Match Engine/stories.cpp
+++ Match Engine/Match Engine/stories.cpp
@@ -1,1 +1,9 @@
 #include "stories.h"
+
+/*
+-input of the damage value by who to who
+-small blurb generated
+-blurb appended to a story
+-at the end of the fight, story is returned.
+
+*/
